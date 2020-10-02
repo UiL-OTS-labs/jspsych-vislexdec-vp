85d70b71aaa61cf4e040906e4773277228e59343
Maarten Duijndam <m.j.a.duijndam@uu.nl>
2020-10-02 12:27:18 +0200

    Add primes to stimuli table

--- stimuli.js
+++ stimuli.js
@@ -1,7 +1,8 @@
 
 // Item types
 const NON_WORD = "NON_WORD";
-const REAL_WORD = "REAL_WORD";
+const UNRELATED = "UNRELATED";
+const RELATED = "RELATED";
 
 const groups = [
     "group1"
@@ -10,20 +11,20 @@ const groups = [
 ];
 
 const practice_items = [
-    {id: 1, item_type: NON_WORD, word: "palve"},
-    {id: 2, item_type: REAL_WORD, word: "hot"}
+    {id: 1, item_type: NON_WORD, word: "palve", prime: "onion"},
+    {id: 2, item_type: UNRELATED, word: "hot", prime: "stapler"}
 ];
 
 const list_group1 = [
-    {id: 1, item_type: NON_WORD, word: "slirque"},
-    {id: 2, item_type: NON_WORD, word: "crawse"},
-    {id: 3, item_type: NON_WORD, word: "twurp"},
-    {id: 4, item_type: NON_WORD, word: "clem"},
+    {id: 1, item_type: NON_WORD, word: "slirque", prime: "eyes"},
+    {id: 2, item_type: NON_WORD, word: "crawse", prime: "piano"},
+    {id: 3, item_type: NON_WORD, word: "twurp", prime: "rabbit"},
+    {id: 4, item_type: NON_WORD, word: "clem", prime: "flower"},
 
-    {id: 5, item_type: REAL_WORD, word: "white"},
-    {id: 6, item_type: REAL_WORD, word: "travel"},
-    {id: 7, item_type: REAL_WORD, word: "letter"},
-    {id: 8, item_type: REAL_WORD, word: "clown"}
+    {id: 5, item_type: RELATED, word: "white", prime: "snow"},
+    {id: 6, item_type: RELATED, word: "travel", prime: "suitcase"},
+    {id: 7, item_type: UNRELATED, word: "letter", prime: "garden"},
+    {id: 8, item_type: UNRELATED, word: "clown", prime: "forest"}
 ];
 
 // Add a second list of stimuli when required.
@@ -37,7 +38,12 @@ const test_items = [
     //{group_name: groups[1], table: list_group2}
 ];
 
-// this function will pick a random group
+// get the list of practise items
+function get_practise_items() {
+    return practice_items;
+}
+
+// this function will pick a random group from the test_items array.
 function pick_random_group() {
     let range = function (n) {
         let empty_array = []
