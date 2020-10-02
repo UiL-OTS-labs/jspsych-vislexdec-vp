d64ba647d2d55880bd7bdfe694a246e01f404e64
Maarten Duijndam <m.j.a.duijndam@uu.nl>
2020-10-02 12:16:29 +0200

    Add support for multiple groups

--- stimuli.js
+++ stimuli.js
@@ -3,12 +3,18 @@
 const NON_WORD = "NON_WORD";
 const REAL_WORD = "REAL_WORD";
 
+const groups = [
+    "group1"
+    // "group2",
+    // "group3"
+];
+
 const practice_items = [
     {id: 1, item_type: NON_WORD, word: "palve"},
     {id: 2, item_type: REAL_WORD, word: "hot"}
 ];
 
-const list_1 = [
+const list_group1 = [
     {id: 1, item_type: NON_WORD, word: "slirque"},
     {id: 2, item_type: NON_WORD, word: "crawse"},
     {id: 3, item_type: NON_WORD, word: "twurp"},
@@ -20,11 +26,26 @@ const list_1 = [
     {id: 8, item_type: REAL_WORD, word: "clown"}
 ];
 
+// Add a second list of stimuli when required.
+// const list_group2 = [
+// ...
+// ]
+
 const test_items = [
-    {group_name: "list_1", table: list_1}
+    {group_name: groups[0], table: list_group1}
+    // Add the second group here, put a comma on the end of the line above here.
+    //{group_name: groups[1], table: list_group2}
 ];
 
+// this function will pick a random group
 function pick_random_group() {
-    let shufarray = [0];
-    return test_items[jsPsych.randomization.repeat(shufarray, 1)];
+    let range = function (n) {
+        let empty_array = []
+        for (var i = 0; i < n; i++) {
+            empty_array.push(i);
+        }
+        return empty_array;
+    }
+    let num_groups = test_items.length;
+    return test_items[jsPsych.randomization.repeat(range(num_groups), 1)];
 }
