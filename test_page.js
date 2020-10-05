52aa2331bd5d4a0232de8dbe8536e53642a0c498
Jacco van Elst <j.c.vanelst@uu.nl>
2020-10-05 17:36:36 +0200

    organisation can indeed be flexible

---
+++ test_page.js
@@ -0,0 +1,8 @@
+  var presentFixation = {
+    type: 'html-keyboard-response',
+    stimulus: '<span style="font-size:40px;">+</span>',
+    choices: jsPsych.NO_KEYS,
+    trial_duration: 200,
+  };
+
+
