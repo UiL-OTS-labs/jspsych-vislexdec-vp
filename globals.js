77f62d7c927eeeda3279684587848392bf66e92b
Jacco van Elst <j.c.vanelst@uu.nl>
2020-10-06 12:49:48 +0200

    pre prime

--- globals.js
+++ globals.js
@@ -1,5 +1,5 @@
 // Default behavior of (sub) trial phases
 const FIXCROSS_DURATION = 400;
-const PRIME_DURATION = 300;
+const PRIME_DURATION = 80;
 const WORD_DURATION = 5000;
 const FEEDBACK_DURATION = 1000;
