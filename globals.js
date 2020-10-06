70cb0b2f6cd6e5bd1843f6d8e7a00960b8e84e85
Jacco van Elst <j.c.vanelst@uu.nl>
2020-10-06 13:42:41 +0200

    loose ends with gaps, placeholders for feedback

--- globals.js
+++ globals.js
@@ -2,9 +2,9 @@
 const FIXCROSS_DURATION = 400;
 const PRIME_DURATION = 80;
 const WORD_DURATION = 5000;
-const FEEDBACK_DURATION = 1000;
+const FEEDBACK_DURATION = 800;
 
-// not using these, but you could
+// if you want gaps, go for it!
 const CROSS_GAP_DURATION = 0;
 const PRIME_GAP_DURATION = 0;
 const WORD_GAP_DURATION = 0;
