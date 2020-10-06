957337e54efbb7884819f894dbd4d37dbc8cb0c5
Jacco van Elst <j.c.vanelst@uu.nl>
2020-10-06 13:10:51 +0200

    feedback, it's always important...

--- globals.js
+++ globals.js
@@ -3,3 +3,11 @@ const FIXCROSS_DURATION = 400;
 const PRIME_DURATION = 80;
 const WORD_DURATION = 5000;
 const FEEDBACK_DURATION = 1000;
+
+// not using these, but you could
+const CROSS_GAP_DURATION = 0;
+const PRIME_GAP_DURATION = 0;
+const WORD_GAP_DURATION = 0;
+
+const TRIAL_GAP_DURATION = 0;
+ 
