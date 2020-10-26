21af0380bfd2db2c47dd733b9aab4ee8d92a98f7
Jacco van Elst <j.c.vanelst@uu.nl>
2020-10-26 15:58:19 +0100

    We may need to discuss this issue further... pushing one of the ways we could have the situation in which: - The Target word is presented for only 1000 ms - Yet the target word phase deadtime for making a response totals 2000ms.
    To discuss:
    - Code-wise/globals.js and strcuture
    - Feedback-wise, eg.
       - do we want dead-time feedback in practice? (please respond as fast
       as you can...")
       - or do we want to add instruction text to meake clear that if the
       feedback (incorrect) comes up without having pressed a key, the
       participant is too slow?

--- globals.js
+++ globals.js
@@ -1,14 +1,25 @@
 // Default behavior of (sub) trial phases
-const FIXCROSS_DURATION = 400;
-const PRIME_DURATION = 80;
-const WORD_DURATION = 5000;
-const FEEDBACK_DURATION = 800;
+const FIXCROSS_DURATION = 1000;
+const PRIME_DURATION = 500;
+const WORD_DURATION = 1000;
 
-// if you want gaps, go for it!
+// having a separate duration for response needs some debate...
+const RESPONSE_TIMEOUT_DURATION = 2000;
+
+// ... One option is to do it this way:
+const RESPONSE_DURATION_OPTION_ONE = 
+    RESPONSE_TIMEOUT_DURATION - WORD_DURATION;
+// ... this will leave us with a value of an extra 1000 ms 
+// as the so-called post trial gap value...
+// but this is not very 'generic'
+
+const FEEDBACK_DURATION = 1000;
+
 const CROSS_GAP_DURATION = 0;
-const PRIME_GAP_DURATION = 0;
+// Duration of the interval between prime and target.
+const PRIME_GAP_DURATION = 300;
 const WORD_GAP_DURATION = 0;
 
-const TRIAL_GAP_DURATION = 0;
+const TRIAL_GAP_DURATION = 1000;
 
 const OK_BUTTON_TEXT = "OK";
