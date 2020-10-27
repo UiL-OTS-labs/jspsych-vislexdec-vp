099b6593c5b6176d43e5998d41a40fe16d6e1d0f
Jacco van Elst <j.c.vanelst@uu.nl>
2020-10-27 12:36:48 +0100

    Added explicit info about time units - set back regular global 2000 interval value of 2000

--- globals.js
+++ globals.js
@@ -3,13 +3,15 @@ const FIXCROSS_DURATION = 1000;
 const PRIME_DURATION = 500;
 const WORD_DURATION = 1000;
 
-const RESPONSE_TIMEOUT_DURATION = 5000;
+const RESPONSE_TIMEOUT_DURATION = 2000;
 
-const FEEDBACK_DURATION = 100;//because i am impatient, testing...
+const FEEDBACK_DURATION = 1000;
 
 const CROSS_GAP_DURATION = 0;
+
 // Duration of the interval between prime and target.
 const PRIME_GAP_DURATION = 300;
+
 const WORD_GAP_DURATION = 0;
 
 const TRIAL_GAP_DURATION = 1000;
