cbf67ffdaed8c5d301283548772b913a26bbe7a4
Jacco van Elst <j.c.vanelst@uu.nl>
2020-11-09 22:01:19 +0100

    POC for a server audio check, this will not work locally...

--- globals.js
+++ globals.js
@@ -13,6 +13,8 @@ const WORD_GAP_DURATION = 0;
 const TRIAL_GAP_DURATION = 1000;
 const OK_BUTTON_TEXT = "OK";
 
-const ACCESS_KEY = 'verysecretsothiswontworkwithoutme';
+const ACCESS_KEY = '382b7538-e7bf-4197-a1a4-73b1351f9198';
 
-const GENERIC_CHECK = "In order to participate in this experiment, please confirm that you:<BR><BR><ul><li>Run this on a Laptop or Desktop computer, <b>not</b> on a phone or a tablet!</li><li>Have a <b>real</b><kbd>keyboard</kbd> attatched and working.</li><li>Have a <b>mouse</b> and/or <b>trackpad</b> that works</li><li>Have your browser's audio enabled and your volume on.</li></ul><h5>Before you click OK, make sure you have your browser window nice and large!</h5><BR><p>We'll try and help you check some things, is that okay?" 
+const GENERIC_CHECK = "In order to participate in this experiment, please confirm that you:<BR><BR><ul><li>Run this on a Laptop or Desktop computer, <b>not</b> on a phone or a tablet!</li><li>Have a <b>real</b><kbd>keyboard</kbd> attatched and working.</li><li>Have a <b>mouse</b> and/or <b>trackpad</b> that works</li><li>Have your browser's audio enabled and your volume on.</li></ul><h5>Before you click OK, make sure you have your browser window nice and large!</h5><BR><p>We'll try and help you check some things, is that okay?"
+
+const AUDIO_CHECK_ONE = "Let's test your audio playback, okay?<BR><BR>Are you alone in a room? It would be best to use your headphones, if you have them, but otherwise: please don't be in a place where some sound from your device would be really annoying<BR><BR>Click 'play sound' to start playing a test sound."
