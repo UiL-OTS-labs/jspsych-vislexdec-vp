8c3df07b839ace2bb4b59b23bc4b054625b5477e
Jacco van Elst <j.c.vanelst@uu.nl>
2020-11-09 22:10:53 +0100

    self-explanatory commit, in the grand scheme of being human, let's talk about key setups and public gits

--- globals.js
+++ globals.js
@@ -13,7 +13,7 @@ const WORD_GAP_DURATION = 0;
 const TRIAL_GAP_DURATION = 1000;
 const OK_BUTTON_TEXT = "OK";
 
-const ACCESS_KEY = '382b7538-e7bf-4197-a1a4-73b1351f9198';
+const ACCESS_KEY = 'of course i will make a mistake';
 
 const GENERIC_CHECK = "In order to participate in this experiment, please confirm that you:<BR><BR><ul><li>Run this on a Laptop or Desktop computer, <b>not</b> on a phone or a tablet!</li><li>Have a <b>real</b><kbd>keyboard</kbd> attatched and working.</li><li>Have a <b>mouse</b> and/or <b>trackpad</b> that works</li><li>Have your browser's audio enabled and your volume on.</li></ul><h5>Before you click OK, make sure you have your browser window nice and large!</h5><BR><p>We'll try and help you check some things, is that okay?"
 
