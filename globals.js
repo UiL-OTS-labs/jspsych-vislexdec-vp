d0cd9856b0de4595432dc32aa916fde133e5d611
Jacco van Elst <j.c.vanelst@uu.nl>
2020-11-10 12:17:50 +0100

    working audio test flow...

--- globals.js
+++ globals.js
@@ -15,6 +15,6 @@ const OK_BUTTON_TEXT = "OK";
 
 const ACCESS_KEY = 'of course i will make a mistake';
 
-const GENERIC_CHECK = "In order to participate in this experiment, please confirm that you:<BR><BR><ul><li>Run this on a Laptop or Desktop computer, <b>not</b> on a phone or a tablet!</li><li>Have a <b>real</b><kbd>keyboard</kbd> attatched and working.</li><li>Have a <b>mouse</b> and/or <b>trackpad</b> that works</li><li>Have your browser's audio enabled and your volume on.</li></ul><h5>Before you click OK, make sure you have your browser window nice and large!</h5><BR><p>We'll try and help you check some things, is that okay?"
+const GENERIC_CHECK = "In order to participate in this experiment, please confirm that you:<BR><BR><ul><li>Run this on a Laptop or Desktop computer, <b>not</b> on a phone or a tablet!</li><li>Have a <b>real keyboard</b> working.</li><li>Have a <b>mouse</b> and/or <b>trackpad</b> that works.</li><li>Have your browser's audio enabled and your volume on.</li></ul><h5>Before you click OK, make sure you have your browser window nice and large!</h5><BR><p>We'll try and help you check some additional things..."
 
-const AUDIO_CHECK_ONE = "Let's test your audio playback, okay?<BR><BR>Are you alone in a room? It would be best to use your headphones, if you have them, but otherwise: please don't be in a place where some sound from your device would be really annoying<BR><BR>Click 'play sound' to start playing a test sound."
+const AUDIO_CHECK_ONE = "Let's test your audio playback, okay?<BR><BR>Are you alone in a room? It would be best to use your headphones, if you have them, but otherwise: please don't be in a place where some sound from your device would be really annoying...<BR><BR>Click 'play sound' to start playing a test sound."
