19b7636a355e37a2d4da304f00d0e8b34e9b0fc4
Jacco van Elst <j.c.vanelst@uu.nl>
2020-11-11 18:17:31 +0100

    setting the stage for survey stuff

--- globals.js
+++ globals.js
@@ -15,6 +15,7 @@ const OK_BUTTON_TEXT = "OK";
 
 const ACCESS_KEY = '382b7538-e7bf-4197-a1a4-73b1351f9198';
 
+// TESTING THINGS 
 const GENERIC_CHECK = "In order to participate in this experiment, please confirm that you:<BR><BR><ul><li>Run this on a Laptop or Desktop computer, <b>not</b> on a phone or a tablet!</li><li>Have a <b>real keyboard</b> working.</li><li>Have a <b>mouse</b> and/or <b>trackpad</b> that works.</li><li>Have your browser's audio enabled and your volume on.</li></ul><h5>Before you click OK, make sure you have your browser window nice and large!</h5><BR><p>We'll try and help you check some additional things..."
 
 const AUDIO_CHECK_ONE = "Let's test your audio playback, okay?<BR><BR>Are you alone in a room? It would be best to use your headphones, if you have them, but otherwise: please don't be in a place where some sound from your device would be really annoying...<BR><BR>Click 'play sound' to start playing a test sound."
@@ -25,10 +26,33 @@ const AUDIO_CHECK_PROMPT_TEXT_LOOP = "Please make sure your audio is audible, yo
 
 const KEYBOARD_CHOICES = ['QWERTY','AZERTY','DVORAK','QWERTZ','CUSTOM'];
 
-console.log(jsPsych.ALL_KEYS);
-
 const KEYBOARD_LEFT_KEY = 'A';
 const KEYBOARD_RIGHT_KEY = 'L';
 
-const HAND_QUESTION = "If forced to choose, I consider myself to be: ";
-const HAND_CHOICES = ['Left handed', 'Right handed'];
+// pre survey test, may go later
+// const HAND_QUESTION = "If forced to choose, I consider myself to be: ";
+// const HAND_CHOICES = ['Left handed', 'Right handed'];
+
+//SURVEY
+
+        // // default options for the multi=block
+        // var bilingual_options = ["No","Yes"];
+        // var dyslex_options = ["No", "Yes", "Unsure (not diagnosed)"];
+        // var gender_options = ["Female", "Male"];
+        // var hand_options = ["Left handed", "Right handed"];
+
+//survey (simple, no input validation from within multi-question)
+const PREPARE_FOR_BINARY_SURVEY = "It is important for this experiment that we ask you to answer some questions in a quick survey. This is a quick screening we need, before we (think we) can use your data for this particular experiment<BR><BR>";
+
+const BILINGUAL_QUESTION = "Were you raised to understand and speak more than one language, from when you were an infant and onwards?"
+const BILINGUAL_OPTIONS = ["No","Yes"];
+
+const DYSLEX_QUESTION = "Are you dyslexic?";
+const DYSLEX_OPTIONS = ["No", "Yes"];
+
+const GENDER_QUESTION = "What is your gender?";
+const GENDER_OPTIONS = ["Female", "Male"];
+
+const HAND_QUESTION = "What defines your hand preference best?"
+const HAND_OPTIONS = ["I am mostly left handed", "I am mostly right handed"];
+
