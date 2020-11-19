f50675828008c797f35b23a2247246a9f220c181
Jacco van Elst <j.c.vanelst@uu.nl>
2020-11-19 15:14:00 +0100

    more housekeeping and sanitizing - wipe that code with alcohol - have it wear a public facing mask

--- globals.js
+++ globals.js
@@ -15,15 +15,20 @@ const PRIME_GAP_DURATION = 300;
 const WORD_GAP_DURATION = 0;
 const TRIAL_GAP_DURATION = 1000;
 const OK_BUTTON_TEXT = "OK";
-
 const YES_OR_NO_BUTTON_TEXT = ["Yes", "No"];
-
-const ACCESS_KEY = 'zeeekretkeey';
+const ACCESS_KEY = 'zeeekretkeey'; // don't commit the real key
 
 // GENERIC TESTS/CHECKS
-const GENERIC_CHECK = "In order to participate in this experiment, please confirm that you:<BR><BR><ul><li>Run this on a Laptop or Desktop computer, <b>not</b> on a phone or a tablet!</li><li>Have a <b>real keyboard</b> working.</li><li>Have a <b>mouse</b> and/or <b>trackpad</b> that works.</li><li>Have your browser's audio enabled and your volume on.</li></ul><h5>Before you click OK, make sure you have your browser window nice and large!</h5><BR><p>We'll try and help you check some additional things..."
+const GENERIC_CHECK = "In order to participate in this experiment, please confirm that you:<BR><BR>" +
+	"<ul><li>Run this on a Laptop or Desktop computer, <b>not</b> on a phone or a tablet!" + 
+	"</li><li>Have a <b>real keyboard</b> working.</li><li>Have a <b>mouse</b> and/or <b>trackpad</b>" + 
+	" that works.</li><li>Have your browser's audio enabled and your volume on.</li></ul>" +
+	"<h5>Before you click OK, make sure you have your browser window nice and large!</h5><BR>" +
+	"<p>Next, we will do some more test and checks.";
 
-const AUDIO_CHECK_PROMPT_TEXT_LOOP = "Please make sure your audio is audible, you can play the sound again to adjust your device volume. Once you're happy with the levels, click 'Continue'"
+//unused here
+const AUDIO_CHECK_PROMPT_TEXT_LOOP = "Please make sure your audio is audible, you can play the sound" + 
+    " again to adjust your device volume. Once you're happy with the levels, click 'Continue'";
 
 const PREPARE_FOR_SURVEY = "We will ask you to answer some screening questions in a quick (forced choice) survey.";
 
@@ -32,8 +37,9 @@ const NATIVE_LANGUAGE_QUESTION = 'What is your native language?'
 const YEAR_BORN_QUESTION = 'In what <i>year</i> were you born?';
 const MONTH_BORN_QUESTION = 'In what <i>month</i> were you born? (1 - 12)';
 
-// block
-const BILINGUAL_QUESTION = 'Were you born and raised in a <a href="https://en.wikipedia.org/wiki/Multilingualism" target="_blank">multilingual</a> environment?'
+// survey multiple choice block
+const BILINGUAL_QUESTION = 'Were you born and raised in a ' + 
+    '<a href="https://en.wikipedia.org/wiki/Multilingualism" target="_blank">multilingual</a> environment?';
 const BILINGUAL_OPTIONS = ["No","Yes"];
 
 const DYSLEXIC_QUESTION = 'Are you <a href="https://en.wikipedia.org/wiki/Dyslexia" target="_blank">dyslexic</a>?';
@@ -42,15 +48,12 @@ const DYSLEXIC_OPTIONS = ["No", "Yes"];
 const GENDER_QUESTION = 'What is your <a href="https://en.wikipedia.org/wiki/Gender" target="_blank">gender</a>?';
 const GENDER_OPTIONS = ["Female", "Male"];
 
-const HAND_QUESTION = 'Which hand do you prefer to write with?'
+const HAND_QUESTION = 'Which hand do you prefer to write with?';
 const HAND_OPTIONS = ["Left", "Right"];
 
 // for which we have some defaults configured
 const KEYBOARD_CHOICES = ['QWERTY','AZERTY','DVORAK','QWERTZ','CUSTOM'];
 
-const KEYBOARD_FALLBACK = 'CUSTOM';
-
-//now for finding out the real bits..
 // simple layout for left/right keys given some known layouts
 
 // Type     |   Left  |   Right
