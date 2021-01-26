3fc6cd5b00f8a0a196ad40d902d05dadc846f7b4
Jacco van Elst <j.c.vanelst@uu.nl>
2021-01-26 14:26:44 +0100

    removed UU styling example. made an effort to layout all text to the left with a most basic .survey class, so it does not inherit jspsych's default centering 'jspsych-content' _if_ that html is refeferenced with that <div class='survey'>    )

--- survey.js
+++ survey.js
@@ -11,157 +11,18 @@ const PREPARE_FOR_SURVEY = "<h1>Please answer some questions first</h1>";
 // might be solved with milestone with things discussed here:
 // https://github.com/jspsych/jsPsych/issues/554#event-3434758022 
 
-const SURVEY_HTML_PLUGIN_STYLE_UU = `
-    <style>
-        body {
-            background: rgb(246, 246, 246);
-            font-family: "Open Sans","Frutiger",Helvetica,Arial,sans-serif;
-            color: rgb(33, 37, 41);
-            text-align: left;
-        }
 
-        p {
-            line-height: 1.4; /* Override paragraph for better readability */
-            text-align: left;
-        }
+// with other survey plugin(s) than html, UU styling is more of a  hassle
+// we should discuss css presets for UU/UiL at some point.
 
-        label {
-            float: left;
-            width: 15em;
-            margin-right: 1em;
-            text-align: left;
-            margin-bottom: 2%;
-        }
-
-        h1, h2{
-            font-size: 2rem;
-            text-align: left;
-        }
-
-        h6 {
-            font-size: 1.1rem;
-            text-align: left;
-        }
-
-        /* Input styles */
-
-        form > table th {
-            padding-left: 10px;
-            vertical-align: middle;
-        }
-
-        input, textarea, select {
-            border-radius: 0;
-            border: 1px solid #d7d7d7;
-            padding: 5px 10px;
-            line-height: 20px;
-            font-size: 16px;
-            text-align: left;
-        }
-
-        input[type=submit], input[type=button], button, .button, .jspsych-btn {
-            background: #000;
-            color: #fff;
-            border: none;
-            font-weight: bold;
-            font-size: 15px;
-            padding: 0 20px;
-            line-height: 42px;
-            width: auto;
-            min-width: auto;
-            cursor: pointer;
-            display: inline-block;
-            border-radius: 0;
-            text-align: left;
-        }
-
-        input[type="checkbox"], input[type="radio"]
-        {
-            width: auto;
-            text-align: left;
-        }
-
-        button[type=submit], input[type=submit], .button-colored {
-            background: #ffcd00;
-            color: #000000;
-        }
-
-        button[type=submit].button-black, input[type=submit].button-black {
-            background: #000;
-            color: #fff;
-        }
-
-        button a, .button a,
-        button a:hover, .button a:hover,
-        a.button, a.button:hover {
-            color: #fff;
-            text-decoration: none;
-        }
-
-        .button-colored a,
-        .button-colored a:hover,
-        a.button-colored,
-        a.button-colored:hover {
-            color: #000;
-        }
-
-        /* Table styles */
-        table thead th {
-            border-bottom: 1px solid #ccc;
-        }
-
-        table tfoot th {
-            border-top: 1px solid #ccc;
-        }
-
-        table tbody tr:nth-of-type(odd) {
-            background: #eee;
-        }
-
-        table tbody tr:hover {
-            background: #ddd;
-        }
-
-        table tbody tr.no-background:hover, table tbody tr.no-background {
-            background: transparent;
-        }
-
-        table tbody td, table thead th, table tfoot th {
-            padding: 6px 5px;
-        }
-
-        /* Link styles */
-        a {
-            color: rgb(33, 37, 41);
-            text-decoration: underline;
-            transition: 0.2s ease color;
-        }
-
-        a:hover {
-            transition: 0.2s ease color;
-            color: rgb(85, 85, 95);
-        }
-
-        span::after {
-            padding-left: 1%;
-        }
-
-        input:invalid + span::after {
-            content: '✖';
-        }
-
-        input:valid+span::after {
-            content: '✓';
-        }
-    </style>
-    `;
-
-// with other survey plugin(s) than html, UU styling seems to not be easy to implement
 const MULTI_CHOICE_HTML =`
+    <div class="survey">
+
     <label for="birth_year">In what year were you born? </label>
     <input type="number" id="birth_year" 
         name="birth_year" placeholder=1999 min=1919 max=2019 required>
     <span class="validity"></span>
+
 
     <br>
     <br>
@@ -180,6 +41,7 @@ const MULTI_CHOICE_HTML =`
     <span class="validity"></span>
     <br> 
     <br> 
+    </div>
     `
 
 // these constants are used in the survery multip[le choice block]
@@ -248,7 +110,7 @@ let survey_multi_choice_block = {
 let survey_multi_html_block = {
     type: 'survey-html-form',
     preamble: PREPARE_FOR_SURVEY,
-    html: SURVEY_HTML_PLUGIN_STYLE_UU + MULTI_CHOICE_HTML,
+    html: MULTI_CHOICE_HTML,
     on_finish: function(data){
         var survey_html_responses = data.responses;
         data.survey_html_responses = survey_html_responses;
@@ -278,6 +140,7 @@ let survey_review_survey_data = {
 
         return `
             <h1>Your data</h1>
+            <div class='survey'>
 
             <div><strong>Birth year</strong>: ${b_year} </div>
             <div><strong>Birth month</strong>: ${b_month} </div>
@@ -289,6 +152,7 @@ let survey_review_survey_data = {
             <br><br>
 
             <p>Is this information correct?</p>
+            </div>
             `;
     },
     choices: [YES_BUTTON_TEXT, NO_BUTTON_TEXT],
