6ca2f2fb181ff8f4fd193cc86dcf729df2b80789
Jacco van Elst <j.c.vanelst@uu.nl>
2021-01-27 13:45:25 +0100

    Moved some code to instructions, fixed https://github.com/UiL-OTS-labs/jspsych-vislexdec-vp/issues/24

--- globals.js
+++ globals.js
@@ -33,25 +33,3 @@ const BAIL_OUT_MOBILE_TEXT = "Please run this experiment on a PC or Laptop."
 // Deafult restrictions of minimal browser dimensions
 const MIN_WIDTH = 800;
 const MIN_HEIGHT = 600;
-
-// GENERIC TESTS/CHECKS
-const GENERIC_CHECK = `
-    <h3>In order to participate, please make sure that you:</h3>
-    <br>
-    <ul>
-      <li>Run this on a Laptop or Desktop computer, <b>not</b> on a phone or a tablet!</li>
-      <li>Have a <b>real keyboard</b> working.</li>
-      <li>Have a <b>mouse</b> and/or <b>trackpad</b> that works.</li>
-    </ul>
-    <h3>Please <i>maximize</i> your browser window before you continue!</h3>
-    <br>
-    <p>Click below if you are ready to proceed</p>
-    `;
-    
-const DEBRIEF_MESSAGE = `
-    <h1>End of the experiment</h1><BR><BR>
-    <h2>Thank you for participating!</h2>
-    `;
-
-const DEBRIEF_MESSAGE_DURATION = 3000;
-
